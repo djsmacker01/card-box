0afa122e02c4b2bafcecb92bbfc70f3f1caa70b1
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-14 17:42:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import './App.css'
+// import Content from './components/Content';
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem",
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering",
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)",
+  ],
+];
+
+
+function App() {
+  const [activeContent, setActiveContent] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>
+          <img src="react-logo.png" alt="React Logo" />
+          <div>
+            <h1>React.js</h1>
+            <p>i.e., using the React library for rendering the UI</p>
+          </div>
+        </header>
+
+        <div id="tabs">
+          <menu>
+            <button
+              className={activeContent === 0 ? "active" : " "}
+              onClick={() => setActiveContent(0)}>
+              Why React?
+            </button>
+
+            <button
+              className={activeContent === 1 ? "active" : " "}
+              onClick={() => setActiveContent(1)}>
+              Core Features
+            </button>
+
+            <button
+              className={activeContent === 2 ? "active" : " "}
+              onClick={() => setActiveContent(2)}>
+              Related Resources
+            </button>
+          </menu>
+
+          <div id='tab-content'>
+            <ul>
+              {content[activeContent].map((item) => (
+                <li key={item}>{ item}</li>
+               ))}
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Content({content}) {
+  
+}
+
+export default Content
